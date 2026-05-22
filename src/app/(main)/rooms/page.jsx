4b29fc7f763920f004ee4ca0a4e900a27ae9a1c3
mursahaul islam noor <mursahaulnur17@gmail.com>
2026-05-22 10:53:01 +0600

    added the all rooms section where all rooms card will dispaly

--- src/app/(main)/rooms/page.jsx
+++ src/app/(main)/rooms/page.jsx
@@ -1,10 +1,24 @@
+import RoomCard from '@/components/RoomCard';
 import React from 'react';
 
-const AllRooms = () => {
+const AllRooms = async () => {
+
+    const res = await fetch('http://localhost:5000/all-rooms');
+    const rooms = await res.json();
+    console.log(rooms);
+
     return (
-        <div>
-            this is the all rooms page;
+       <div className="text-[#1a661d]">
+      <div className="container mx-auto py-8">
+        <h1 className="text-center text-3xl font-bold mb-3">All Rooms</h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {rooms.map((room) => {
+            return <RoomCard key={room._id} room={room}></RoomCard>
+          })}
         </div>
+      </div>
+    </div>
     );
 };
 
